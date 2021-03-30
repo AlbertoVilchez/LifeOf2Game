70534eef50d341d4e0cf6158a8f1d1494841cc22
DeusDuck <60322308+AlbertoVilchez@users.noreply.github.com>
2021-03-30 02:17:41 +0200

    Diseño total Ciudad

---
+++ Life2/Assets/Scripts/CameraPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPlayer : MonoBehaviour
+{
+    public GameObject Player;
+
+    Vector3 DirrecionRelative;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DirrecionRelative = transform.position - Player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void LateUpdate()
+    {
+       
+
+        transform.position = Player.transform.position + DirrecionRelative;
+    }
+}
